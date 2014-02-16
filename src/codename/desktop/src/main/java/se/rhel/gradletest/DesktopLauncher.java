33f292c159bc6067bd10a6f40ac408ed82dfe4b5
Emil Lindau <emillindau@gmail.com>
2014-02-16 19:23:41 +0100

    Borde fungera som startup!

---
+++ src/codename/desktop/src/main/java/se/rhel/gradletest/DesktopLauncher.java
@@ -0,0 +1,13 @@
+
+package se.rhel.gradletest;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.rhel.gradletest.HelloApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HelloApp(), config);
+	}
+}
