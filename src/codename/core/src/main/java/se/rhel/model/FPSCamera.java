b69bc6e166d0833c389c7248e50694276a00146a
Emil Lindau <emillindau@gmail.com>
2014-02-25 13:25:30 +0100

    Nytt vapen, ny animering

--- src/codename/core/src/main/java/se/rhel/model/FPSCamera.java
+++ src/codename/core/src/main/java/se/rhel/model/FPSCamera.java
@@ -12,7 +12,7 @@ public class FPSCamera extends PerspectiveCamera {
     GameObject mObj;
 
     public static final Vector3 UP = new Vector3(0,1,0);
-    private final Vector3 mOffset = new Vector3(0, 1f, 0f);
+    private final Vector3 mOffset = new Vector3(0, 1.8f, 0f);
 
     /**
      *
@@ -35,12 +35,15 @@ public class FPSCamera extends PerspectiveCamera {
     public void update() {
 
         mObj.getTransformation().getTranslation(position);
+        // Move the camera to be in tha face
         position.add(mOffset);
 
         // TODO: Well, this is obviously not that good
-        position.sub(getForward().scl(0.5f));
-        position.sub(getRight().scl(1.5f));
-        position.add(this.up.scl(1f));
+        position.sub(getForward().scl(0.4f));
+        // position.sub(getRight().scl(1.5f));
+
+
+        // position.add(this.up.scl(1f));
 
         super.update();
     }
