4beb82841a5726f5c2488a2b48ff620eaea63578
Emil Lindau <emillindau@gmail.com>
2014-03-05 19:39:01 +0100

    Test commit from IntelliJ

--- src/codename/core/src/main/java/se/rhel/model/client/ClientWorldModel.java
+++ src/codename/core/src/main/java/se/rhel/model/client/ClientWorldModel.java
@@ -1,8 +1,8 @@
 package se.rhel.model.client;
 
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import se.rhel.client.Client;
-import se.rhel.server.Server;
 import se.rhel.model.BaseModel;
 import se.rhel.model.BulletWorld;
 import se.rhel.model.FPSCamera;
@@ -11,10 +11,11 @@ import se.rhel.model.Player;
 public class ClientWorldModel implements BaseModel {
 
     private FPSCamera mCamera;
-    private Player mPlayer;
+    private Player mLocalPlayer;
     private BulletWorld mBulletWorld;
 
     private Client mClient;
+    private Array<Player> mPlayers;
 
     public ClientWorldModel(Client client) {
         mClient = client;
@@ -25,8 +26,9 @@ public class ClientWorldModel implements BaseModel {
     public void create() {
         mBulletWorld = new BulletWorld();
         mCamera = new FPSCamera(75, 0.1f, 1000f);
-        mPlayer = new Player(new Vector3(0, 10, 0), mBulletWorld);
-        mPlayer.attachCamera(mCamera);
+        mLocalPlayer = new Player(new Vector3(0, 10, 0), mBulletWorld);
+        mLocalPlayer.attachCamera(mCamera);
+        mPlayers.add(mLocalPlayer);
     }
 
     @Override
@@ -37,12 +39,12 @@ public class ClientWorldModel implements BaseModel {
     @Override
     public void update(float delta) {
         mBulletWorld.update(delta);
-        mPlayer.update(delta);
+        mLocalPlayer.update(delta);
     }
 
     public BulletWorld getBulletWorld() { return mBulletWorld; }
     public Player getPlayer() {
-        return mPlayer;
+        return mLocalPlayer;
     }
     public FPSCamera getCamera() { return mCamera; }
 
