ce383ded4e09a6ed7433598e8ac63b4375cb7c33
Emil Lindau <emillindau@gmail.com>
2014-03-17 15:18:11 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/codename/core/src/main/java/se/rhel/model/client/ClientWorldModel.java
    	src/codename/core/src/main/java/se/rhel/model/server/ServerWorldModel.java
    	src/codename/network/src/se/rhel/packet/Packet.java

--- src/codename/core/src/main/java/se/rhel/network/packet/MyPacketRegisterInitializer.java
+++ src/codename/core/src/main/java/se/rhel/network/packet/MyPacketRegisterInitializer.java
@@ -1,5 +1,6 @@
 package se.rhel.network.packet;
 
+import se.rhel.network.PlayerMovePacket;
 import se.rhel.packet.PacketManager;
 import se.rhel.packet.TestMaxPacket;
 import se.rhel.packet.TestPacket;
